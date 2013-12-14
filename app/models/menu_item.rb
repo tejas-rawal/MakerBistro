6a8a311c8afb1cfa7ada9964720182a087402f8a
Tejas Rawal <trawal90@gmail.com>
2013-12-14 15:18:45 -0600

    Add haml to view files. Add seed data. Create routes.

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,4 +1,5 @@
 class MenuItem < ActiveRecord::Base
 has_many :recipe
 has_many :ingredients, through: :recipes
+validates :name, :price, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+menu_items = [
+  { name: "Breakfast Burrito",
+    price: 2.75
+  },
+  { name: "Bagel w. Cream Cheese",
+    price: 2.25
+  },
+  { name: "Vegan Burger",
+    price: 7.75
+  },
+  { name: "BLT",
+    price: 6.25
+  },
+  { name: "Fettucine Pasta",
+    price: 6.75
+  }
+]
+
+puts "Creating Menu Items"
+menu_items.each do |item|
+  MenuItem.create(item)
+end
