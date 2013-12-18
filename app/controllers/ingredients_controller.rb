871dc0e157ccf70ce5fa9e9bda97a88041fc6419
Tejas Rawal <trawal90@gmail.com>
2013-12-17 21:48:28 -0600

    Add ingredient to menu item and make association in recipe model.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,17 +1,19 @@
 class IngredientsController < ApplicationController
 
   def create
-    @ingredient = Ingredient.new(ingredient_params)
-
+    @menu_item = MenuItem.find(params[:menu_item_id])
+    @ingredient = @menu_item.ingredients.new(ingredient_params)
+      
     respond_to do |format|
       if @ingredient.save
-        format.html { redirect_to @ingredient.menu_item, notice: 'Ingredient was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @ingredient.menu_item }
+        format.html { redirect_to @menu_item, notice: 'Ingredient was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @menu_item }
       else
         format.html { render action: 'new' }
         format.json { render json: @ingredient.errors, status: :unprocessable_entity }
       end
     end
+    @menu_item.ingredients << @ingredient
   end
 
 
