5fe91fe7c973b855f109f1aa5cb8a742aec1c6ee
Tejas Rawal <trawal90@gmail.com>
2013-12-18 15:26:55 -0600

    Add conditional which checks to see if ingredient being added already exists.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,18 +2,28 @@ class IngredientsController < ApplicationController
 
   def create
     @menu_item = MenuItem.find(params[:menu_item_id])
-    @ingredient = @menu_item.ingredients.new(ingredient_params)
-      
-    respond_to do |format|
-      if @ingredient.save
+    ingredient = Ingredient.find_by_name(params[:ingredient][:name])
+    
+    if ingredient
+      @ingredient = ingredient
+      respond_to do |format|
+        @menu_item.ingredients << @ingredient
         format.html { redirect_to @menu_item, notice: 'Ingredient was successfully created.' }
         format.json { render action: 'show', status: :created, location: @menu_item }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @ingredient.errors, status: :unprocessable_entity }
-      end
+      end 
+    else
+      @ingredient = @menu_item.ingredients.new(ingredient_params)
+      respond_to do |format|
+        if @ingredient.save
+          @menu_item.ingredients << @ingredient
+          format.html { redirect_to @menu_item, notice: 'Ingredient was successfully created.' }
+          format.json { render action: 'show', status: :created, location: @menu_item }
+        else
+          format.html { render action: 'new' }
+          format.json { render json: @ingredient.errors, status: :unprocessable_entity }
+        end
+      end 
     end
-    @menu_item.ingredients << @ingredient
   end
 
 
